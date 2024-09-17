4724ac50171aed063b0ee9358089fcbf353db692
Tamás <molnarj@elg-bp.edu.hu>
2024-09-17 12:10:47 +0200

    létrehoztam az etelek menüpontot

---
+++ resources/views/etelek.blade.php
@@ -0,0 +1,3 @@
+<div>
+Helló ételek
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+#ez a sor mondja meg, hogy használni akarom az etelController nevű osztályt:
+use App\Http\Controllers\etelController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+#itt ténylegesen meghatározom az url-t
+Route::get('/etelek',[etelController::class,"index"]);
