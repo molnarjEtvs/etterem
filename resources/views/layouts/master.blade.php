dcc20e0e4984d0c1b32245397cac08b71bbffc71
Balog Vivien <balogvivien04@gmail.com>
2024-09-17 15:58:13 +0200

    kész: Vivi

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,6 +24,10 @@
                             <li class="nav-item">
                                 <a class="nav-link text-white" href="{{route('etelek')}}">Ételek</a>
                             </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{route('vivi')}}">Vivi</a>
+                            </li>
                             
                           </ul>
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 #ez a sor mondja meg, hogy használni akarom az etelController nevű osztályt:
 use App\Http\Controllers\etelController;
+use App\Http\Controllers\ViviController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,5 @@ Route::get('/', function () {
 
 #itt ténylegesen meghatározom az url-t
 Route::get('/etelek',[etelController::class,"index"])->name('etelek');
+
+Route::get('/vivi',[ViviController::class,"index"])->name('vivi');
